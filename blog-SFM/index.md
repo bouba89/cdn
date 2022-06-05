d48ea1a757b278895ff34d698078e6c973ce0aa5
Ko5t1C <kos.wanalike@gmail.com>
2022-06-05 14:27:43 +0200

    [ADD] python generate MarkDown files and more

---
+++ blog-SFM/index.md
@@ -0,0 +1,4 @@
+[blog-SFM/bkg.png](./blog-SFM/bkg.png). 
+[blog-SFM/p10k_vmdeb.png](./blog-SFM/p10k_vmdeb.png). 
+[blog-SFM/zsh_first_usage.png](./blog-SFM/zsh_first_usage.png). 
+[blog-SFM/bullet.png](./blog-SFM/bullet.png). 
---
+++ cdn.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import sys,os,glob
+#print(os.listdir(sys.argv[1]))
+
+def error_exit ():
+	print('Error: an argument (Folder) is require for running script !')
+	print('type ./' + str(sys.argv[0]) + ' <arg>' )
+	sys.exit()
+
+if __name__ ==  '__main__':
+	''' DEBUG
+	'''
+	'''
+	print("Nom du fichier : ", sys.argv[0])
+	print("Nombre d'arguments: ", len(sys.argv))
+	print("Liste des arguments: " , str(sys.argv))
+	'''
+	''' END DEBUG
+	'''
+	if len(sys.argv) > 1 and len(sys.argv) < 3:
+	
+		folder = sys.argv[1]
+		print("\nList of files and folders in " + folder + ':\n')
+		print(os.listdir(folder))
+		# search all files inside a specific folder
+		# *.* means file name with any extension
+		print('\n glob list files and folder recursively \n')
+		directory = "./"
+		pathname = folder + "/**/*.*"
+		for file in glob.glob(pathname, recursive=True):
+			print(file)
+			#export to html like file but it's a markdown  .md for github
+			filename =  folder + '/index.md'	
+			if os.path.exists(filename):
+				append_write = 'a' # append if already exists
+			else:
+				append_write = 'w' # make a new file if not
+
+			markdown = open(filename,append_write)
+			#[Link to another page](./another-page.html).
+			markdown.write("[" + file + "](./" + file + "). \n")
+			markdown.close()
+
+	else:
+		error_exit()
